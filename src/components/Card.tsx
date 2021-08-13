c4c3e264b4cf85cd2fbbab2bbbc2be6f24479484
bh070 <bh070@hdm-stuttgart.de>
2021-08-13 19:08:32 +0200

    Add basic Card Layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,32 @@
-import { IonCard, IonCardContent } from "@ionic/react";
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/react";
 import React, {Component} from "react";
+import '../CSS/Card.css';
 
 class Card extends Component {
+    constructor (){
+        super(true);
+        this.state= {
+            side: 'choosing'
+        }
+    }
+
     render () {
         return (
+            
             <IonCard>
-                <IonCardContent>
-                    Hey
-                </IonCardContent>
-            </IonCard>
+          <IonCardHeader>
+            <IonCardTitle style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>Truth/Dare</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent >
+              <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+            Frage
+            </div>
+            
+      </IonCardContent>
+        </IonCard>
+
+          
         )
     }
 }
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import Card from '../components/Card';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <Card/>
+      <IonContent>
+        <Card />
+        <IonGrid id="grid">
+      <IonRow>
+        <IonCol style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+          <IonButton>
+
+          </IonButton>
+        </IonCol>
+        <IonCol style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+         <IonButton>
+            
+          </IonButton>
+        </IonCol>
+        <IonCol style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+        <IonButton>
+            
+        </IonButton>
+        </IonCol>
+      </IonRow>
+      
+    </IonGrid>
       </IonContent>
     </IonPage>
   );
